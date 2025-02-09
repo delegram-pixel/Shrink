82a88704212d5fe3fa53dcaf208648f0bb98c88d
Dele <bamideleaug2002@gmail.com>
2025-02-09 11:24:36 +0100

    done

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,17 @@
+import {
+  
+    SignIn,
+   
+  } from '@clerk/nextjs'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      
+      <SignIn/>
+
+    
+    </div>
+  )
+}
